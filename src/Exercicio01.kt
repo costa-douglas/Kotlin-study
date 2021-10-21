42c333b3f0530c5e9e01d865f93309a24b376a4c
Douglas P. Costa <53386017+costa-douglas@users.noreply.github.com>
2021-10-21 20:13:48 -0300

    Update Exercicio01.kt

--- src/Exercicio01.kt
+++ src/Exercicio01.kt
@@ -1,7 +1,7 @@
 //Crie uma variável global que indique o percentual de reajuste de um  determinado produto,
 //que deve ser calculado a partir do método main definido  na função.
 //Calcule o novo salário e exiba-o em console.
-/*
+
 val reajuste = 20.0
 class Salario(salarioBase: Double, reajuste: Double) {
     val salarioBase: Double
@@ -21,4 +21,4 @@ fun main() {
     var novoSalario = Salario(2000.0, reajuste)
     println(novoSalario.calReajuste())
 }
-*/
+
